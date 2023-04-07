eb990e3f58b0d5baaac0eadc1bbd31d40e968050
The5w2h <srivastava.tanuj@outlook.com>
2023-04-07 11:46:44 +0530

    First Commit for DreamFlats project

---
+++ DreamFlats/Controllers/DreamFlatsAPIController.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DreamFlats.Controllers
+{
+    public class DreamFlatsAPIController : ControllerBase
+    {
+        public DreamFlatsAPIController()
+        {
+        }
+    }
+}
+
